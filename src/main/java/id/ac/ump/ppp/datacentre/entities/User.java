00277f19b8b7113daf45f2ee0be619234cfe5372
mikeybestdrummer <fani.triastowo@gmail.com>
2015-03-12 13:28:22 +0700

    add entity User and Persistence

---
+++ src/main/java/id/ac/ump/ppp/datacentre/entities/User.java
@@ -0,0 +1,21 @@
+package id.ac.ump.ppp.datacentre.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}

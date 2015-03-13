ea4bb6f8a800c5a7d2d2907d92efd0b2907f4c9a
Fani Triastowo <fani.triastowo@gmail.com>
2015-03-13 11:13:42 +0700

    add Role.java

--- src/main/java/id/ac/ump/ppp/datacentre/entities/User.java
+++ src/main/java/id/ac/ump/ppp/datacentre/entities/User.java
@@ -1,8 +1,12 @@
 package id.ac.ump.ppp.datacentre.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class User {
@@ -11,6 +15,30 @@ public class User {
 	@GeneratedValue
 	private Integer id;
 
+	private String username;
+
+	private String password;
+
+	@ManyToMany
+	@JoinTable
+	private List<Role> roles;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -18,4 +46,13 @@ public class User {
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
 }

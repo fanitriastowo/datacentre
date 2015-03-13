ba33c0aa167a7d01061b3a57dee31ae85029ee8a
Fani Triastowo <fani.triastowo@gmail.com>
2015-03-13 14:22:15 +0700

    add RoleRepo and UserRepo

---
+++ src/main/java/id/ac/ump/ppp/datacentre/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package id.ac.ump.ppp.datacentre.repositories;
+
+import id.ac.ump.ppp.datacentre.entities.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}

4ebf54673392f0946ff65db99ce10f0bb6a7a9b2
mikeybestdrummer <fani.triastowo@gmail.com>
2015-03-29 22:04:27 +0700

    add Lantai entity and Lantai Repository

---
+++ src/main/java/id/ac/ump/ppp/datacentre/entities/Lantai.java
@@ -0,0 +1,55 @@
+package id.ac.ump.ppp.datacentre.entities;
+
+import id.ac.ump.ppp.datacentre.entities.kondisi.Kondisi;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "LANTAI")
+public class Lantai {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private Double keramikPanjang;
+
+	private String keramikBahan;
+
+	private Kondisi keramikKondisi;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Double getKeramikPanjang() {
+		return keramikPanjang;
+	}
+
+	public void setKeramikPanjang(Double keramikPanjang) {
+		this.keramikPanjang = keramikPanjang;
+	}
+
+	public String getKeramikBahan() {
+		return keramikBahan;
+	}
+
+	public void setKeramikBahan(String keramikBahan) {
+		this.keramikBahan = keramikBahan;
+	}
+
+	public Kondisi getKeramikKondisi() {
+		return keramikKondisi;
+	}
+
+	public void setKeramikKondisi(Kondisi keramikKondisi) {
+		this.keramikKondisi = keramikKondisi;
+	}
+}
